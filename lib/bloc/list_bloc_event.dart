21a77ebe712faccf63033b7d99d39955e1856aab
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-02-27 12:09:58 +0500

    architecture setup done

---
+++ lib/bloc/list_bloc_event.dart
@@ -0,0 +1,4 @@
+part of 'list_bloc_bloc.dart';
+
+@immutable
+sealed class ListBlocEvent {}
---
+++ lib/bloc/list_bloc_state.dart
@@ -0,0 +1,6 @@
+part of 'list_bloc_bloc.dart';
+
+@immutable
+sealed class ListBlocState {}
+
+final class ListBlocInitial extends ListBlocState {}
